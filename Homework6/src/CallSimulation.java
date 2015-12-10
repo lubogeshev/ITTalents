c280ffc7d6d085df26a265d3c2302a924b9b8b08
Lubo <Lubo@Lubo-PC>
2015-12-10 12:38:01 +0200

    Call simulation created

---
+++ Homework6/src/CallSimulation.java
@@ -0,0 +1,16 @@
+
+public class CallSimulation {
+
+	public static void main(String[] args) {
+		
+		GSM nokia = new GSM();
+		nokia.insertSimCard("0883459022");
+		
+		GSM samsung = new GSM();
+		samsung.insertSimCard("0899933669");
+		
+		
+		
+
+	}
+}
