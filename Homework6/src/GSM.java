09bfa46ac2fbe24b1670df4b39ce69d7a66bee66
Lubo <Lubo@Lubo-PC>
2015-12-10 10:47:13 +0200

    Created insterSimCard method

--- Homework6/src/GSM.java
+++ Homework6/src/GSM.java
@@ -7,7 +7,14 @@ public class GSM {
 	int outgoingCallsDuration;
 	int lastIncomingCall;
 	int lastOutgoingCall;
-	
-	
+
+	void insertSimCard(String phoneNumber){
+		if (phoneNumber.length() == 10 && phoneNumber.charAt(0) ==0 && phoneNumber.charAt(1) ==8)	{
+			hasSimCard = true;
+		}
+		else {
+			System.out.println("Invalid phone number!");
+		}
+	}
 	
 }
