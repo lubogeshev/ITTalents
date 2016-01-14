60cd2d6a892909984523bada46872050a35149fe
Lubomir Geshev <lubo_geshev@abv.bg>
2016-01-14 16:57:42 +0200

    added homework 7 source files

---
+++ Homework 7/src/Computer/Computer.java
@@ -0,0 +1,57 @@
+package Computer;
+
+public class Computer {
+
+	int year;
+	double price;
+	boolean isNotebook;
+	double hardDiskMemory;
+	double freeMemory;
+	String operationSystem;
+	String model;
+
+	public Computer() {
+		isNotebook = false;
+		operationSystem = "Win XP";
+	}
+
+	public Computer(int year, double price, double hardDiskMemory, double freeMemory) {
+		this();
+		this.year = year;
+		this.price = price;
+		this.hardDiskMemory = hardDiskMemory;
+		this.freeMemory = freeMemory;
+	}
+
+	public Computer(int year, double price, boolean isNotebook, double hardDiskMemory, double freeMemory,
+			String operationSystem) {
+		this(year, price, hardDiskMemory, freeMemory);
+		this.isNotebook = isNotebook;
+		this.operationSystem = operationSystem;
+	}
+
+	void changeOperationSystem(String newOperationSystem) {
+		this.operationSystem = newOperationSystem;
+	}
+
+	void useMemory(int memory) {
+		if ((this.freeMemory - memory) > 0)
+			this.freeMemory -= memory;
+		else
+			System.out.println("Not enough free memory!");
+	}
+
+	byte comparePrice (Computer c){
+		if (this.price > c.price){
+			return -1;
+		}
+		else if (this.price < c.price){
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+
+}
