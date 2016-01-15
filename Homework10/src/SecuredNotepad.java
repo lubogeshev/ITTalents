07396c60193194ef5eefcf5b098e4a65ae39c56e
Lubomir Geshev <lubo_geshev@abv.bg>
2016-01-15 12:56:50 +0200

    DelText and ViewPage added in SecuredNotepad class

--- Homework10/src/SecuredNotepad.java
+++ Homework10/src/SecuredNotepad.java
@@ -7,9 +7,9 @@ public class SecuredNotepad extends SimpleNotepad {
 		super(numberOfPages);
 		this.password = password;
 	}
-	
+
 	public void addText(Page page, String newText, String password) {
-		if (this.password.equals(password)){
+		if (this.password.equals(password)) {
 			super.addText(page, newText);
 		} else {
 			System.out.println("Invalid password. Page not added!");
@@ -17,10 +17,26 @@ public class SecuredNotepad extends SimpleNotepad {
 	}
 
 	public void replaceText(Page page, String newText, String password) {
-		if (this.password.equals(password)){
+		if (this.password.equals(password)) {
 			super.replaceText(page, newText);
 		} else {
-			System.out.println("Invalid password. Page not added!");
+			System.out.println("Invalid password. Text not added!");
+		}
+	}
+
+	public void delText(Page page, String password) {
+		if (this.password.equals(password)) {
+			super.delText(page);
+		} else {
+			System.out.println("Invalid password. Text not deleted!");
+		}
+	}
+	
+	public void viewPage(String password) {
+		if (this.password.equals(password)) {
+			super.viewPage();
+		} else {
+			System.out.println("Invalid password. Can not show text!");
 		}
 	}
 }
