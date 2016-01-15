abb99dd09f3ed60ad1ac3e05aa5e6ab9a1200116
Lubomir Geshev <lubo_geshev@abv.bg>
2016-01-16 01:43:15 +0200

    searchWord and  containsDigits methods ovverrided

--- Homework10/src/Page.java
+++ Homework10/src/Page.java
@@ -7,10 +7,10 @@ public class Page {
 	public Page(String title, String text) {
 		if (title == null) {
 			this.title = "";
-			
+
 		} else if (text == null) {
 			this.text = "";
-			
+
 		} else {
 			this.title = title;
 			this.text = text;
@@ -21,7 +21,7 @@ public class Page {
 		if (newText == null) {
 			newText = "";
 		}
-		this.text = this.text.concat (newText);
+		this.text = this.text.concat(newText);
 	}
 
 	void delText() {
@@ -30,5 +30,20 @@ public class Page {
 
 	String viewPage() {
 		return (this.title + "\n" + this.text);
+	}
+
+	boolean searchWord(String word) {
+		if (word != null) {
+			return this.text.toLowerCase().contains(word.toLowerCase());
+		}
+		System.out.println("No word to search for!");
+		return false;
+	}
+
+	boolean containsDigits() {
+		if (this.text.matches(".*\\d+.*")) {
+			return true;
+		}
+		return false;
 	}
 }
