49b546d27ef1c67e424fe0680224b755563b5d41
Lubomir Geshev <lubo_geshev@abv.bg>
2016-01-15 12:45:36 +0200

    StringBuilders back to Strings

--- Homework10/src/Page.java
+++ Homework10/src/Page.java
@@ -1,18 +1,31 @@
 
 public class Page {
-	
-	StringBuilder title;
-	StringBuilder text;
-	
-	void addText (String newText){
-		text.append(newText);
+
+	String title;
+	String text;
+
+	public Page(String title, String text) {
+		if (title == null) {
+			this.title = "";
+			
+		} else if (text == null) {
+			this.text = "";
+			
+		} else {
+			this.title = title;
+			this.text = text;
+		}
 	}
-	
-	void delText(){
-		text.delete(0, text.length()-1);
+
+	void addText(String newText) {
+		this.text.concat (newText);
 	}
-	
-	void viewPage (){
+
+	void delText() {
+		this.text = "";
+	}
+
+	void viewPage() {
 		System.out.println(title);
 		System.out.println(text);
 	}
--- Homework10/src/SecuredNotepad.java
+++ Homework10/src/SecuredNotepad.java
@@ -1,12 +1,26 @@
 
-public class SecuredNotepad extends SimpleNotepad{
-
-	public SecuredNotepad(int numberOfPages) {
-		super(numberOfPages);
-		// TODO Auto-generated constructor stub
-	}
+public class SecuredNotepad extends SimpleNotepad {
 
 	String password;
 
+	public SecuredNotepad(int numberOfPages, String password) {
+		super(numberOfPages);
+		this.password = password;
+	}
+	
+	public void addText(Page page, String newText, String password) {
+		if (this.password.equals(password)){
+			super.addText(page, newText);
+		} else {
+			System.out.println("Invalid password. Page not added!");
+		}
+	}
 
+	public void replaceText(Page page, String newText, String password) {
+		if (this.password.equals(password)){
+			super.replaceText(page, newText);
+		} else {
+			System.out.println("Invalid password. Page not added!");
+		}
+	}
 }
