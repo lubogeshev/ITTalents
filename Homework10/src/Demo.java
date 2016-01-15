0d34b29a0139d04fa763af3ca2caf215b54f962a
Lubomir Geshev <lubo_geshev@abv.bg>
2016-01-15 15:39:27 +0200

    Demo simplified

--- Homework10/src/Demo.java
+++ Homework10/src/Demo.java
@@ -4,8 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		SecuredNotepad taenDnevnik = new SecuredNotepad(20, "");
-		taenDnevnik.pages[2].title = "TITLE 2";
-		taenDnevnik.pages[19].text = "SAMO ";
 		taenDnevnik.addText(19, "LEVSKI");
 		taenDnevnik.viewPage();
 		
