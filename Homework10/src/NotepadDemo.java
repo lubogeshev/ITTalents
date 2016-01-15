6619577200d89c8656fdd6d7fd3433dff6a861b8
Lubomir Geshev <lubo_geshev@abv.bg>
2016-01-16 00:09:01 +0200

    DEMO renamed and widened

---
+++ Homework10/src/NotepadDemo.java
@@ -0,0 +1,29 @@
+
+public class NotepadDemo {
+
+	public static void main(String[] args) {
+		
+		SecuredNotepad taenDnevnik = new SecuredNotepad(5, "");
+		SimpleNotepad notepad = new SimpleNotepad(4);
+		
+		notepad.addText(1, "text on page 1");
+		notepad.viewPage();
+		
+		taenDnevnik.addText(3, "test text");
+		taenDnevnik.replaceText(3, "replacement");
+		taenDnevnik.delText(3);
+		taenDnevnik.viewPage();
+		
+		Page lastPage = new Page("Last page", "I te taka...");
+		System.out.println(lastPage.viewPage());
+		
+		INotepad x = new SimpleNotepad(3);
+		x.viewPage();
+		x = new SecuredNotepad(4, "");
+		x.viewPage();
+		
+	}
+
+	
+	
+}
